c538c9da445b03d9b93c5dc274ece740c5aa3b64
Zach <zachcshore@gmail.com>
2022-06-14 07:20:04 -0500

    increase list

--- src/main/java/com/jagrosh/jmusicbot/commands/music/LocalSearchCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/music/LocalSearchCmd.java
@@ -16,6 +16,36 @@ public final class LocalSearchCmd extends SearchCmd {
         this.aliases = bot.getConfig().getAliases(this.name);
     }
 
-	
+	@Override
+        public void playlistLoaded(AudioPlaylist playlist)
+        {
+            builder.setColor(event.getSelfMember().getColor())
+                    .setText(FormatUtil.filter(event.getClient().getSuccess()+" Search results for `"+event.getArgs()+"`:"))
+                    .setChoices(new String[0])
+                    .setSelection((msg,i) -> 
+                    {
+                        AudioTrack track = playlist.getTracks().get(i-1);
+                        if(bot.getConfig().isTooLong(track))
+                        {
+                            event.replyWarning("This track (**"+track.getInfo().title+"**) is longer than the allowed maximum: `"
+                                    +FormatUtil.formatTime(track.getDuration())+"` > `"+bot.getConfig().getMaxTime()+"`");
+                            return;
+                        }
+                        AudioHandler handler = (AudioHandler)event.getGuild().getAudioManager().getSendingHandler();
+                        int pos = handler.addTrack(new QueuedTrack(track, event.getAuthor()))+1;
+                        event.replySuccess("Added **" + FormatUtil.filter(track.getInfo().title)
+                                + "** (`" + FormatUtil.formatTime(track.getDuration()) + "`) " + (pos==0 ? "to begin playing" 
+                                    : " to the queue at position "+pos));
+                    })
+                    .setCancel((msg) -> {})
+                    .setUsers(event.getAuthor())
+                    ;
+            for(int i=0; i<10 && i<playlist.getTracks().size(); i++)
+            {
+                AudioTrack track = playlist.getTracks().get(i);
+                builder.addChoices("`["+FormatUtil.formatTime(track.getDuration())+"]` [**"+track.getInfo().title+"**]("+track.getInfo().uri+")");
+            }
+            builder.build().display(m);
+        }
 
 }
